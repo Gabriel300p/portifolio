a4cdc4c5b2210635998b7a7937c1b439a02901e2
Gabriel Andrade <gabriel.andrade200p@gmail.com>
2022-01-20 15:05:50 -0300

    1.0.3

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -65,3 +65,17 @@ tabs.forEach(tab => {
     tab.classList.add('qualification_active')
   })
 })
+
+// * Portfolio Swiper
+let swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+});
